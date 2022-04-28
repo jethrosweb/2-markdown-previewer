4d36850dccb59fd49ea6e547ce089d94ae251176
Jethro Dean <jethroldean@gmail.com>
2022-04-28 13:51:55 +1000

    Markdown working, no style yet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
 import React from "react"
 import "./App.scss"
+import { marked } from "marked"
+
+const initialState = `
+This is a paragraph
+**This is bolded text**
+> Block quotes!
+
+# Heading
+## Heading 2
+
+- list item 1
+- list item 2
+- list item 3
+
+[Visit my website](link here)
+
+This is an inline \`<div></div>\`
+
+This is a block code
+
+\`\`\`
+let x = 1;
+let x = 2;
+let z = x + y;
+\`\`\`
+
+![React](link here)
+`
+
+marked.setOptions({
+    breaks: true
+})
+
+const renderer = new marked.Renderer()
 
 export default function App() {
+
+    const [text, setText] = React.useState(initialState)
+
+    const handleChange = (event) => {
+        setText(event.target.value)
+    }
+
     return (
-        <h1>Hello World</h1>
+        <div className="container">
+            <textarea id="editor" value={text} onChange={handleChange}/>
+            <Preview markdown={text} />
+        </div>
+    )
+}
+
+function Preview({ markdown }) {
+    return (
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdown, {renderer: renderer})}}></div>
     )
 }
